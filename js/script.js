b232baf1a13e6eda18a3c6cbca2fe5d8abca14e4
DhruvDutta <54740423+DhruvDutta@users.noreply.github.com>
2020-06-26 20:21:11 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(
+    function(){
+        if(window.innerWidth<window.innerHeight*.75){
+            console.log(document.getElementById('tuf').width,$('#tuf').width())
+        var tuf=$('#tuf')
+        tuf.css('left',`-${1556/2}px`)
+        }
+
+    }
+);
+
