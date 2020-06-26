a2dd28cdbb0c4eae069e55ab16ad3a9c3863f288
DhruvDutta <54740423+DhruvDutta@users.noreply.github.com>
2020-06-26 22:13:34 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(
     function(){
         if(window.innerWidth<window.innerHeight*.75){
-            console.log(document.getElementById('tuf').width,$('#tuf').width())
+        
         var tuf=$('#tuf')
         tuf.css('left',`-${1556/2}px`)
         }
