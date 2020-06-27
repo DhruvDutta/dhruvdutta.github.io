42b93722e5ac51071d57304f61ac29658a4a6343
DhruvDutta <54740423+DhruvDutta@users.noreply.github.com>
2020-06-27 19:19:22 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,36 +5,57 @@ $(
         tuf.css('margin-left',`-${$('#tuf').width()/2}px`)
         }
         
-    
+        if(window.innerWidth<1000){
+            for(let j = 1;j <=10;j++){
+                $(`#${j}`).addClass('col-12');
+
+               
+            }
+        }
     }
 );
 
 let ic;
 function slide(){
     var back = document.getElementById('tuf');
-    back.style.transition = 'all 1.5s ease-in-out';
+    back.style.transition = 'all 1.2s ease-in-out';
     back.style.marginTop = '-100vh';
     var page = document.getElementById('dhruv');
-    page.style.transition = `all 1.5s ease-in-out`;
+    page.style.transition = `all 1.2s ease-in-out`;
     page.style.marginTop = '-100vh';
     $('.fbtn,.foot').hide();
     setTimeout(function(){
         $('#tuf,#first-page').hide();
         $('.main').css({'display':'block','animation-play-state':'running'});
         ic = setInterval(icons_appear,700);
-    },1400)
+    },1100)
 
 }
 
 let i = 1;
+let icons=['profile','bio','1','2','3','4','5','6','7','8','9','10']
+
 function icons_appear(){
-        if(i>10){
+        if(i>12){
             clearInterval(ic)
         }else{  
-            var icon = $(`#${i}`);
+            var icon = $(`#${icons[i]}`);
             icon.css('animation-play-state','running')
             i+=1;
         }
 
     
+}
+
+function icons_appear_random(){
+    let r= Math.ceil(Math.random()*10);
+    console.log(r)
+    if(icons.length==10){
+        clearInterval(ic);
+    }
+    if(icons.includes(r)){
+        icons_appear_random();
+    }else{
+            $(`#${r}`).css('animation-play-state','running');
+    }
 }
